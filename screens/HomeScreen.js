8c248095328d1b777aab9a9615c26c53ff946a89
gashnick <gashnicolas@gmail.com>
2024-01-04 14:56:15 +0200

    Creating mobile app

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,12 +15,16 @@ import {
 import { styles } from "../theme";
 import TrendingMovies from "../components/trendingMovies";
 import MovieList from "../components/movieList";
+import { useNavigation } from "@react-navigation/native";
+import Loading from "../components/loading";
 
 const ios = Platform.OS == "ios";
 export default function HomeScreen() {
   const [trending, setTrending] = useState([1, 2, 3]);
   const [upcoming, setUpcoming] = useState([1, 2, 3]);
   const [topRated, setTopRated] = useState([1, 2, 3]);
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
   return (
     <View className="flex-1 bg-neutral-800">
       <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
@@ -31,26 +35,31 @@ export default function HomeScreen() {
           <Text className="text-white text-3xl font-bold">
             <Text style={styles.text}>M</Text>ovies
           </Text>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Search")}>
             <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
           </TouchableOpacity>
         </View>
       </SafeAreaView>
-      <ScrollView
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 10 }}
-      >
-        {/* Trending movies carousel */}
-        <TrendingMovies data={trending} />
 
-        {/* upcoming movies */}
+      {loading ? (
+        <Loading />
+      ) : (
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 10 }}
+        >
+          {/* Trending movies carousel */}
+          <TrendingMovies data={trending} />
 
-        <MovieList title="Upcoming" data={upcoming} />
+          {/* upcoming movies */}
 
-        {/* Toprated movies */}
+          <MovieList title="Upcoming" data={upcoming} />
 
-        <MovieList title="Toprated" data={upcoming} />
-      </ScrollView>
+          {/* Toprated movies */}
+
+          <MovieList title="Toprated" data={upcoming} />
+        </ScrollView>
+      )}
     </View>
   );
 }
